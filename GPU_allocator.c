1f87a5f451bcb793d533d7ea37bf9571f4529313
Avisha Kumar <ak754@cornell.edu>
2019-10-01 23:12:14 -0400

    add

--- GPU_allocator.c
+++ GPU_allocator.c
@@ -21,7 +21,7 @@ void gi_init(struct gpu_info *gi){
 	memset(gi->allocated, 0, sizeof(gi->allocated));
 	gi->nfree = NGPUS;
 	rthread_sema_init(&gi->mutex_procure, 0);
-	rthread_sema_init(&gi->mutex_track, 0);
+	rthread_sema_init(&gi->mutex_track, 1);
 	rthread_sema_init(&gi->sema, 0);
 	rthread_sema_init(&gi->overallocation_sema, 0);
 }
